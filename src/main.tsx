133b2fce84b2e02104ae1a9fbdb95253c9ecfaac
himanushi <himanushi@users.noreply.github.com>
2023-06-20 07:38:03 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { setupIonicReact } from "@ionic/react";
 
-setupIonicReact({ mode: "ios" });
+setupIonicReact({ mode: "md" });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
