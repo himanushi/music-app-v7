8b315dded0421f0986303d730f5bc2f8b41292d5
himanushi <himanushi@users.noreply.github.com>
2023-06-19 16:59:17 +0900

    add albums page

---
+++ src/pages/albums/albums.tsx
@@ -0,0 +1,14 @@
+import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+
+export const Albums = () => {
+  return (
+    <IonContent>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Albums</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      albums
+    </IonContent>
+  );
+};
