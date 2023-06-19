a71f8c6cd0878c3c35ee4c4512be94d8e9f6276b
himanushi <himanushi@users.noreply.github.com>
2023-06-19 16:37:20 +0900

    add ionic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { IonApp, IonPage, IonRouterOutlet } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { Route } from "react-router-dom";
+
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
 
@@ -14,19 +18,19 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
+import { Albums } from "./pages/albums/albums";
+
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <IonApp>
+      <IonPage>
+        <IonReactRouter>
+          <IonRouterOutlet>
+            <Route path="/" component={Albums} />
+          </IonRouterOutlet>
+        </IonReactRouter>
+      </IonPage>
+    </IonApp>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { setupIonicReact } from "@ionic/react";
+
+setupIonicReact();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
