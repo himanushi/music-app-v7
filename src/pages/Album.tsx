192b1563b3a3531d3746a96861ab4ac64a35897f
himanushi <himanushi@users.noreply.github.com>
2023-06-26 07:43:49 +0900

    add virtual scroll

--- src/pages/Album.tsx
+++ src/pages/Album.tsx
@@ -8,18 +8,37 @@ import {
   IonCol,
   IonGrid,
   IonRow,
+  IonList,
+  IonItemDivider,
+  IonItem,
+  IonLabel,
+  IonNote,
+  IonAvatar,
 } from "@ionic/react";
+import { useEffect, useRef, useState } from "react";
+import { Virtuoso } from "react-virtuoso";
 
 export const Album = () => {
+  const contentElement = useRef<HTMLIonContentElement>();
+  const [scrollElement, setScrollElement] = useState<HTMLElement>();
+
+  useEffect(() => {
+    (async () => {
+      if (contentElement.current) {
+        setScrollElement(await contentElement.current.getScrollElement());
+      }
+    })();
+  }, [contentElement]);
+
   return (
     <IonPage>
-      <IonContent>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Album</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonGrid>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Album</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent ref={contentElement as any}>
+        <IonGrid class="">
           <IonRow>
             <IonCol>
               <IonImg
@@ -33,6 +52,44 @@ export const Album = () => {
             </IonCol>
           </IonRow>
         </IonGrid>
+        <IonList>
+          <IonItemDivider>Album</IonItemDivider>
+          <IonItem className="ion-text-wrap">
+            タイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          </IonItem>
+          <IonItem className="ion-text-wrap">会社名</IonItem>
+          <IonItem>
+            <IonLabel>配信日</IonLabel>
+            <IonNote>2023年6月23日</IonNote>
+          </IonItem>
+          <IonItem>
+            <IonLabel>曲数</IonLabel>
+            <IonNote>100曲</IonNote>
+          </IonItem>
+          <IonItem>
+            <IonLabel>再生時間</IonLabel>
+            <IonNote>1時間</IonNote>
+          </IonItem>
+        </IonList>
+
+        <Virtuoso
+          useWindowScroll
+          customScrollParent={scrollElement}
+          style={{ height: "100%" }}
+          totalCount={100}
+          itemContent={(index) => {
+            return (
+              <div style={{ height: "44px" }}>
+                <IonItem>
+                  <IonAvatar slot="start">
+                    <img src="https://picsum.photos/seed/picsum/40/40" />
+                  </IonAvatar>
+                  <IonLabel>{index}</IonLabel>
+                </IonItem>
+              </div>
+            );
+          }}
+        />
       </IonContent>
     </IonPage>
   );
