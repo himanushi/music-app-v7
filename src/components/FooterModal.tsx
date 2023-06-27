b66523c8f3ec2b5e5b8283c5d28af03393cb717e
himanushi <himanushi@users.noreply.github.com>
2023-06-28 06:11:37 +0900

    add close player

--- src/components/FooterModal.tsx
+++ src/components/FooterModal.tsx
@@ -8,39 +8,75 @@ import {
   IonTitle,
 } from "@ionic/react";
 import { Icon } from ".";
+import { useCallback, useRef, useState } from "react";
+import type {
+  IonModalCustomEvent,
+  ModalBreakpointChangeEventDetail,
+} from "@ionic/core";
+
+const min = 0.1;
+const max = 0.95;
 
 export const FooterModal = () => {
+  const modal = useRef<HTMLIonModalElement>(null);
+  const [open, setOpen] = useState(false);
+  const switchBreakpoint = useCallback(() => {
+    modal.current?.setCurrentBreakpoint(open ? min : max);
+    setOpen((prevOpen) => !prevOpen);
+  }, [open]);
+  const bcreakpointDidChange = useCallback(
+    (event: IonModalCustomEvent<ModalBreakpointChangeEventDetail>) => {
+      setOpen(event.detail.breakpoint === max);
+    },
+    []
+  );
+
   return (
     <IonModal
+      ref={modal}
       isOpen={true}
-      initialBreakpoint={0.1}
-      breakpoints={[0.1, 0.95]}
+      initialBreakpoint={min}
+      breakpoints={[min, max]}
       backdropDismiss={false}
-      backdropBreakpoint={0.9}
+      backdropBreakpoint={max}
+      onIonBreakpointDidChange={bcreakpointDidChange}
     >
       <IonContent forceOverscroll={false}>
-        <IonItem color="black" lines="none">
-          <IonThumbnail>
-            <img src={`https://picsum.photos/id/101/300`} />
-          </IonThumbnail>
-          <IonTitle>
-            タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
-          </IonTitle>
-          <IonButtons slot="end">
-            <IonButton>
-              <Icon size="s" color="white" slot="icon-only" name="play_arrow" />
-            </IonButton>
-            <IonButton>
-              <Icon
-                size="s"
-                color="white"
-                slot="icon-only"
-                name="fast_forward"
-              />
-            </IonButton>
-          </IonButtons>
-        </IonItem>
+        {open ? (
+          <OpenPlayer />
+        ) : (
+          <ClosePlayer switchBreakpoint={switchBreakpoint} />
+        )}
       </IonContent>
     </IonModal>
   );
+};
+
+const ClosePlayer = ({
+  switchBreakpoint,
+}: {
+  switchBreakpoint: () => void;
+}) => {
+  return (
+    <IonItem onClick={switchBreakpoint} color="black" lines="none">
+      <IonThumbnail>
+        <img src={`https://picsum.photos/id/101/300`} />
+      </IonThumbnail>
+      <IonTitle>
+        タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+      </IonTitle>
+      <IonButtons slot="end">
+        <IonButton>
+          <Icon size="s" color="white" slot="icon-only" name="play_arrow" />
+        </IonButton>
+        <IonButton>
+          <Icon size="s" color="white" slot="icon-only" name="fast_forward" />
+        </IonButton>
+      </IonButtons>
+    </IonItem>
+  );
+};
+
+const OpenPlayer = () => {
+  return <></>;
 };
