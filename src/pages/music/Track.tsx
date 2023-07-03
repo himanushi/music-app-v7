ee73078c818535bf4d4fdb2e04ce03679e43604e
himanushi <himanushi@users.noreply.github.com>
2023-07-04 06:54:58 +0900

    track 詳細画面のアーティストとアルバムを追加

--- src/pages/music/Track.tsx
+++ src/pages/music/Track.tsx
@@ -18,11 +18,15 @@ import {
   IonSkeletonText,
   IonThumbnail,
 } from "@ionic/react";
-import { Fragment, useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { Virtuoso } from "react-virtuoso";
 import { FooterPadding, Icon, SquareImage } from "~/components";
-import { ArtistsDocument, TrackDocument, TrackObject } from "~/graphql/types";
+import {
+  AlbumsDocument,
+  ArtistsDocument,
+  TrackDocument,
+} from "~/graphql/types";
 import { useScrollElement } from "~/hooks";
 import { convertImageUrl, convertTime, toMs } from "~/lib";
 
@@ -60,20 +64,22 @@ export const Track: React.FC<
         </IonGrid>
         <IonList>
           <IonItem className="ion-text-wrap text-select" lines="none">
+            {track ? <IonLabel>{track.name}</IonLabel> : <IonSkeletonText />}
+          </IonItem>
+          <IonItem className="ion-text-wrap text-select" lines="none">
             {track ? (
               <IonNote slot="end">{convertTime(toMs([track]))}</IonNote>
             ) : (
               <IonSkeletonText />
             )}
           </IonItem>
+          <IonItemDivider sticky>トラック</IonItemDivider>
           <IonItem button detail={false}>
             {track ? (
               <>
-                <IonButtons slot="start">
-                  <IonButton>
-                    <Icon size="m" slot="icon-only" name="play_arrow" />
-                  </IonButton>
-                </IonButtons>
+                <IonThumbnail slot="start">
+                  <img src={track.artworkM?.url} />
+                </IonThumbnail>
                 <IonLabel class="ion-text-wrap">{track.name}</IonLabel>
                 <IonButtons slot="end">
                   <IonButton>
@@ -94,105 +100,32 @@ export const Track: React.FC<
             )}
           </IonItem>
         </IonList>
-        {/* {album ? (
-          <AlbumTracks
-            tracks={tracks as TrackObject[]}
-            scrollElement={scrollElement}
-          />
-        ) : (
-          <IonList>
-            <IonItemDivider sticky>トラック</IonItemDivider>
-            {[...Array(10)].map((_, i) => (
-              <IonItem button detail={false} key={i}>
-                <IonSkeletonText />
-              </IonItem>
-            ))}
-          </IonList>
+        {track && (
+          <TrackArtists trackId={track.id} scrollElement={scrollElement} />
         )}
-        {album ? (
-          <AlbumArtists albumId={album.id} scrollElement={scrollElement} />
-        ) : (
-          <IonList>
-            <IonItemDivider sticky>アーティスト</IonItemDivider>
-            {[...Array(5)].map((_, i) => (
-              <IonItem button detail={false} key={i}>
-                <IonSkeletonText />
-              </IonItem>
-            ))}
-          </IonList>
-        )} */}
+        {track && (
+          <TrackAlbums trackId={track.id} scrollElement={scrollElement} />
+        )}
         <FooterPadding />
       </IonContent>
     </IonPage>
   );
 };
 
-const AlbumTracks = ({
-  tracks,
+const TrackArtists = ({
+  trackId,
   scrollElement,
 }: {
-  tracks: TrackObject[];
+  trackId: string;
   scrollElement: HTMLElement | undefined;
 }) => {
-  const discTracks = tracks.reduce(
-    (discs: TrackObject[][], track) =>
-      track.trackNumber === 1
-        ? [...discs, [track]]
-        : [...discs.slice(0, -1), [...discs[discs.length - 1], track]],
-    []
-  );
-
-  return (
-    <IonList>
-      {discTracks.map((tracks, i) => (
-        <Fragment key={i}>
-          {discTracks.length >= 2 ? (
-            <IonItemDivider sticky>ディスク {i + 1}</IonItemDivider>
-          ) : (
-            <IonItemDivider sticky>トラック</IonItemDivider>
-          )}
-          <Virtuoso
-            useWindowScroll
-            customScrollParent={scrollElement}
-            style={{ height: "44.5px" }}
-            totalCount={tracks.length}
-            itemContent={(index) => (
-              <IonItem button detail={false}>
-                <IonNote slot="start">{tracks[index].trackNumber}</IonNote>
-                <IonLabel class="ion-text-wrap">{tracks[index].name}</IonLabel>
-                <IonButtons slot="end">
-                  <IonButton>
-                    <Icon
-                      size="s"
-                      color="red"
-                      slot="icon-only"
-                      name="favorite"
-                    />
-                  </IonButton>
-                </IonButtons>
-              </IonItem>
-            )}
-          />
-        </Fragment>
-      ))}
-    </IonList>
-  );
-};
-
-const AlbumArtists = ({
-  albumId,
-  scrollElement,
-}: {
-  albumId: string;
-  scrollElement: HTMLElement | undefined;
-}) => {
-  const limit = 10;
+  const limit = 50;
 
   const [offset, setOffset] = useState(limit);
 
   const { data, fetchMore } = useQuery(ArtistsDocument, {
     variables: {
-      conditions: { albumIds: [albumId] },
+      conditions: { trackIds: [trackId] },
       cursor: { limit, offset: 0 },
       sort: { direction: "DESC", order: "POPULARITY" },
     },
@@ -227,6 +160,70 @@ const AlbumArtists = ({
             <IonAvatar slot="start" style={{ height: "60px", width: "60px" }}>
               <img src={`https://picsum.photos/id/${index + 100}/600`} />
             </IonAvatar>
+            <IonLabel>{artists[index].name}</IonLabel>
+            <IonButtons slot="end">
+              <IonButton>
+                <Icon size="s" color="red" slot="icon-only" name="favorite" />
+              </IonButton>
+            </IonButtons>
+          </IonItem>
+        )}
+      />
+    </IonList>
+  );
+};
+
+const TrackAlbums = ({
+  trackId,
+  scrollElement,
+}: {
+  trackId: string;
+  scrollElement: HTMLElement | undefined;
+}) => {
+  const limit = 50;
+
+  const [offset, setOffset] = useState(limit);
+
+  const { data, fetchMore } = useQuery(AlbumsDocument, {
+    variables: {
+      conditions: { trackIds: [trackId] },
+      cursor: { limit, offset: 0 },
+      sort: { direction: "DESC", order: "POPULARITY" },
+    },
+    fetchPolicy: "cache-first",
+  });
+
+  const fetchItems = useCallback(
+    (offset: number) => {
+      setOffset((prevOffset) => prevOffset + limit);
+      fetchMore({
+        variables: {
+          cursor: { limit, offset: offset },
+        },
+      });
+    },
+    [fetchMore]
+  );
+
+  const artists = useMemo(() => data?.items ?? [], [data?.items]);
+
+  return (
+    <IonList>
+      <IonItemDivider sticky>アルバム</IonItemDivider>
+      <Virtuoso
+        useWindowScroll
+        customScrollParent={scrollElement}
+        style={{ height: "44.5px" }}
+        totalCount={artists.length}
+        endReached={() => fetchItems(offset)}
+        itemContent={(index) => (
+          <IonItem button detail={false}>
+            <IonThumbnail
+              slot="start"
+              style={{ height: "110px", width: "110px" }}
+            >
+              <img src={artists[index].artworkM?.url} />
+            </IonThumbnail>
             <IonLabel>{artists[index].name}</IonLabel>
             <IonButtons slot="end">
               <IonButton>
