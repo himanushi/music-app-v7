6a138b1b0c4543d56924f2d22ef105d201b7a58c
himanushi <himanushi@users.noreply.github.com>
2023-07-05 18:27:45 +0900

    サイズ調整

--- src/components/FooterPadding.tsx
+++ src/components/FooterPadding.tsx
@@ -6,6 +6,7 @@ export const FooterPadding = () => {
       <IonItem lines="none" />
       <IonItem lines="none" />
       <IonItem lines="none" />
+      <IonItem lines="none" />
     </>
   );
 };
