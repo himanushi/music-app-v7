46fdc4896b997befdaae74fb9c57f1f9c6fc97ee
himanushi <himanushi@users.noreply.github.com>
2023-07-06 07:44:28 +0900

    add login page

---
+++ src/pages/settings/Login.tsx
@@ -0,0 +1,104 @@
+import { useMutation } from "@apollo/client";
+import {
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  useIonToast,
+} from "@ionic/react";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Icon } from "~/components";
+import { LoginDocument, MeDocument } from "~/graphql/types";
+import { useErrorMessages } from "~/hooks";
+
+export const Login = () => {
+  const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [login, { data, error }] = useMutation(LoginDocument, {
+    refetchQueries: [{ query: MeDocument }],
+  });
+  const { errorMessages } = useErrorMessages(error);
+  const [present, dismiss] = useIonToast();
+
+  // login success
+  useEffect(() => {
+    if (data?.login) {
+      history.push("/settings");
+      (async () => {
+        await dismiss();
+        await present({
+          message: "ログインしました",
+          duration: 5000,
+          position: "middle",
+          color: "main",
+        });
+      })();
+    }
+  }, [data?.login, dismiss, errorMessages, history, present]);
+
+  // login error
+  useEffect(() => {
+    if (errorMessages && errorMessages["_"]) {
+      (async () => {
+        await dismiss();
+        await present({
+          message: errorMessages["_"][0],
+          duration: 5000,
+          position: "middle",
+          color: "red",
+        });
+      })();
+    }
+  }, [dismiss, errorMessages, present]);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar />
+      </IonHeader>
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle style={{ paddingBottom: "0px" }} size="large">
+              ログイン
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonList>
+          <IonItem>
+            <IonInput
+              label="ユーザーID"
+              labelPlacement="stacked"
+              type="email"
+              onIonInput={(e) => setUsername(e.detail.value!)}
+            ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput
+              label="パスワード"
+              labelPlacement="stacked"
+              type="password"
+              onIonInput={(e) => setCurrentPassword(e.detail.value!)}
+            ></IonInput>
+          </IonItem>
+          <IonItem
+            button
+            onClick={() =>
+              login({ variables: { input: { username, currentPassword } } })
+            }
+          >
+            <Icon name="login" slot="start" color="blue" />
+            <IonLabel>ログイン</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
