8dc894342414720f16c68b92b234033168a4b4c7
himanushi <himanushi@users.noreply.github.com>
2023-07-10 11:28:13 +0900

    apple music の情報表示

--- src/components/LogoIcon.tsx
+++ src/components/LogoIcon.tsx
@@ -8,12 +8,26 @@ export type LogoIconName =
   | "spotify"
   | "youtube";
 
+const sizeMap = {
+  l: 40,
+  m: 32,
+  s: 24,
+};
+
 export const LogoIcon = ({
   name,
   slot,
+  size = "m",
 }: {
   name: LogoIconName;
   slot: "start" | "end" | "icon-only";
+  size?: "s" | "m" | "l";
 }) => {
-  return <IonIcon slot={slot} src={`/assets/logo-${name}.svg`} />;
+  return (
+    <IonIcon
+      style={{ fontSize: `${sizeMap[size]}px` }}
+      slot={slot}
+      src={`/assets/logo-${name}.svg`}
+    />
+  );
 };
--- src/pages/settings/AppleMusicAccount.tsx
+++ src/pages/settings/AppleMusicAccount.tsx
@@ -3,6 +3,9 @@ import { LogoIcon } from "~/components/LogoIcon";
 import { useStartedServiceState } from "~/hooks";
 import { mergeMeta } from "~/lib";
 import { appleMusicAccountService } from "~/machines";
+import { Capacitor } from "@capacitor/core";
+import { ReactNode } from "react";
+import { Icon } from "~/components";
 
 export const AppleMusicAccount = () => {
   useStartedServiceState(appleMusicAccountService);
@@ -15,10 +18,44 @@ export const AppleMusicAccount = () => {
 
   return (
     <IonList>
-      <IonItem button onClick={onClick}>
+      <IonItem button onClick={onClick} lines="none">
         <LogoIcon name="apple-music" slot="start" />
         <IonLabel>Apple Music {label}</IonLabel>
       </IonItem>
+      <Info />
     </IonList>
+  );
+};
+
+const Info = () => {
+  const service = appleMusicAccountService.getSnapshot();
+
+  const contents: ReactNode[] = [];
+  if (service.matches("authorized") && Capacitor.getPlatform() === "ios") {
+    contents.push(
+      "ログアウトする場合は設定で<strong>メディアと Apple Music</strong>を無効にしてください。"
+    );
+  }
+  if (service.matches("unauthorized") && Capacitor.getPlatform() === "ios") {
+    contents.push(
+      "ログインする場合は設定で<strong>メディアと Apple Music</strong>を有効にしてください。"
+    );
+  }
+  if (service.matches("authorized")) {
+    contents.push(
+      "Apple Music からログアウトした場合は30秒の試聴再生となります。"
+    );
+  }
+  if (service.matches("unauthorized")) {
+    contents.push(
+      "Apple Music にログインすると、フル再生やプレイリスト追加などが出来るようになります。ログインしない場合は30秒の試聴再生となります。"
+    );
+  }
+
+  return (
+    <IonItem>
+      <Icon color="blue" name="info" slot="start" />
+      <IonLabel class="ion-text-wrap">{contents}</IonLabel>
+    </IonItem>
   );
 };
