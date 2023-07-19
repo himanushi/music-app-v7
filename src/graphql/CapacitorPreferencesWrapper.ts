3cc09cb7bfc9f5ab2e380e2f0e180e2ff2af4cf1
himanushi <himanushi@users.noreply.github.com>
2023-07-20 07:28:08 +0900

    apollo client の storage は stringifyされている状態で実行するため修正

--- src/graphql/CapacitorPreferencesWrapper.ts
+++ src/graphql/CapacitorPreferencesWrapper.ts
@@ -3,11 +3,11 @@ import { Preferences } from "@capacitor/preferences";
 export class CapacitorPreferencesWrapper {
   async getItem(key: string) {
     const { value } = await Preferences.get({ key });
-    return value ? JSON.parse(value) : null;
+    return value ? value : null;
   }
 
   setItem(key: string, data: any) {
-    return Preferences.set({ key, value: JSON.stringify(data) });
+    return Preferences.set({ key, value: data });
   }
 
   removeItem(key: string) {
