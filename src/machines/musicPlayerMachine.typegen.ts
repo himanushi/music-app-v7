b784ee8aec412cb8815d2e953fd00ff6c3d85bcf
himanushi <himanushi@users.noreply.github.com>
2023-08-19 10:30:02 +0900

    lint

--- src/machines/musicPlayerMachine.typegen.ts
+++ src/machines/musicPlayerMachine.typegen.ts
@@ -1,43 +1,43 @@
+// This file was automatically generated. Edits will be overwritten
 
-  // This file was automatically generated. Edits will be overwritten
-
-  export interface Typegen0 {
-        '@@xstate/typegen': true;
-        internalEvents: {
-          "xstate.init": { type: "xstate.init" };
-        };
-        invokeSrcNameMap: {
-          
-        };
-        missingImplementations: {
-          actions: never;
-          delays: never;
-          guards: never;
-          services: never;
-        };
-        eventsCausingActions: {
-          "changeCurrentTrack": "NEXT_PLAY" | "PREVIOUS_PLAY" | "REPLACE_AND_PLAY";
-"changeRepeat": "CHANGE_REPEAT";
-"moveTracks": "REORDER";
-"nextPlaybackNo": "NEXT_PLAY";
-"pause": "PLAY_OR_PAUSE";
-"play": "PLAY_OR_PAUSE";
-"previousPlaybackNo": "PREVIOUS_PLAY";
-"removeTrack": "REMOVE_TRACK";
-"setCurrentPlaybackNo": "REPLACE_AND_PLAY";
-"setTracks": "REPLACE_AND_PLAY";
-        };
-        eventsCausingDelays: {
-          
-        };
-        eventsCausingGuards: {
-          "canNextPlay": "NEXT_PLAY";
-"canPreviousPlay": "PREVIOUS_PLAY";
-        };
-        eventsCausingServices: {
-          
-        };
-        matchesStates: "loading" | "loading.loadingPlay" | "loading.waiting" | "paused" | "playing" | "ready" | "stopped" | { "loading"?: "loadingPlay" | "waiting"; };
-        tags: never;
-      }
-  
+export interface Typegen0 {
+  "@@xstate/typegen": true;
+  internalEvents: {
+    "xstate.init": { type: "xstate.init" };
+  };
+  invokeSrcNameMap: {};
+  missingImplementations: {
+    actions: never;
+    delays: never;
+    guards: never;
+    services: never;
+  };
+  eventsCausingActions: {
+    changeCurrentTrack: "NEXT_PLAY" | "PREVIOUS_PLAY" | "REPLACE_AND_PLAY";
+    changeRepeat: "CHANGE_REPEAT";
+    moveTracks: "REORDER";
+    nextPlaybackNo: "NEXT_PLAY";
+    pause: "PLAY_OR_PAUSE";
+    play: "PLAY_OR_PAUSE";
+    previousPlaybackNo: "PREVIOUS_PLAY";
+    removeTrack: "REMOVE_TRACK";
+    setCurrentPlaybackNo: "REPLACE_AND_PLAY";
+    setTracks: "REPLACE_AND_PLAY";
+  };
+  eventsCausingDelays: {};
+  eventsCausingGuards: {
+    canNextPlay: "NEXT_PLAY";
+    canPreviousPlay: "PREVIOUS_PLAY";
+  };
+  eventsCausingServices: {};
+  matchesStates:
+    | "loading"
+    | "loading.loadingPlay"
+    | "loading.waiting"
+    | "paused"
+    | "playing"
+    | "ready"
+    | "stopped"
+    | { loading?: "loadingPlay" | "waiting" };
+  tags: never;
+}
