18648cc58b6bf1c2ab87be213bc41af027f1b3f6
himanushi <himanushi@users.noreply.github.com>
2023-08-28 18:53:33 +0900

    名前変更

--- src/pages/settings/Me.tsx
+++ src/pages/settings/Me.tsx
@@ -1,8 +1,27 @@
 import { useMutation } from "@apollo/client";
-import { IonItem, IonLabel, IonList, IonNote, useIonToast } from "@ionic/react";
-import { Icon } from "~/components";
-import { CurrentUserObject, LogoutDocument, MeDocument } from "~/graphql/types";
-import { useMe } from "~/hooks";
+import {
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonNote,
+  IonTitle,
+  IonToolbar,
+  useIonRouter,
+  useIonToast,
+} from "@ionic/react";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Icon, Page } from "~/components";
+import {
+  CurrentUserObject,
+  LogoutDocument,
+  MeDocument,
+  UpdateMeDocument,
+} from "~/graphql/types";
+import { useErrorMessages, useMe } from "~/hooks";
 
 export const Me = () => {
   const { me } = useMe();
@@ -39,6 +58,7 @@ const RegisteredMe = ({ me }: { me: CurrentUserObject }) => {
       position: "top",
     });
   };
+  const router = useIonRouter();
 
   return (
     <IonList>
@@ -54,9 +74,98 @@ const RegisteredMe = ({ me }: { me: CurrentUserObject }) => {
         <IonLabel>権限</IonLabel>
         <IonNote slot="end">{me.role.description}</IonNote>
       </IonItem>
+      <IonItem button onClick={() => router.push("/me/edit")}>
+        <Icon name="edit" slot="start" color="green" />
+        <IonLabel>設定変更</IonLabel>
+      </IonItem>
       <IonItem button onClick={onClick}>
         <Icon name="logout" slot="start" color="red" />
         <IonLabel>ログアウト</IonLabel>
+      </IonItem>
+    </IonList>
+  );
+};
+
+export const EditMe = () => {
+  return (
+    <Page>
+      <IonHeader>
+        <IonToolbar />
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle style={{ paddingBottom: "0px" }} size="large">
+              設定変更
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <UpdateName />
+      </IonContent>
+    </Page>
+  );
+};
+
+const UpdateName = () => {
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [update, { data, error }] = useMutation(UpdateMeDocument, {
+    refetchQueries: [{ query: MeDocument }],
+  });
+  const { errorMessages } = useErrorMessages(error);
+  const [present, dismiss] = useIonToast();
+  const { me } = useMe();
+  const router = useIonRouter();
+
+  useEffect(() => {
+    if (me) setName(me.name);
+  }, [me]);
+
+  useEffect(() => {
+    if (data?.updateMe) {
+      (async () => {
+        await dismiss();
+        await present({
+          message: "変更しました",
+          duration: 2000,
+          position: "top",
+          color: "main",
+        });
+        router.push("/settings");
+      })();
+    }
+  }, [data?.updateMe, dismiss, errorMessages, history, present, router]);
+
+  useEffect(() => {
+    if (errorMessages && errorMessages["_"]) {
+      (async () => {
+        await dismiss();
+        await present({
+          message: errorMessages["_"][0],
+          duration: 5000,
+          position: "top",
+          color: "red",
+        });
+      })();
+    }
+  }, [dismiss, errorMessages, present]);
+
+  return (
+    <IonList>
+      <IonItem>
+        <IonInput
+          label="名前"
+          labelPlacement="stacked"
+          onIonInput={(e) => setName(e.detail.value!)}
+          value={name}
+        ></IonInput>
+      </IonItem>
+      <IonItem
+        button
+        onClick={() => update({ variables: { input: { name } } })}
+      >
+        <Icon name="edit" slot="start" color="blue" />
+        <IonLabel>変更</IonLabel>
       </IonItem>
     </IonList>
   );
